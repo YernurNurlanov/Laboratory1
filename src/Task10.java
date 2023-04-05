ab8c932659913ba3dcefd8c5d4ce43ea3d318b06
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-04-05 10:50:04 +0600

    implementation of func which finds GCD

--- src/Task10.java
+++ src/Task10.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Task10 {
     public static void findGCD(){
         Scanner sc = new Scanner(System.in);
+        //input two numbers
         int a = sc.nextInt();
         int b = sc.nextInt();
         if (a>b){
@@ -14,6 +15,7 @@ public class Task10 {
     }
     public static int findGCDFunc(int a, int b){
         if (a % b != 0){
+            //compute GCD of b and the remainder of a divided by b
             b = findGCDFunc(b, a % b);
         }
         return b;
