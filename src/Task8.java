75c05c613359437e0b8b7f28de6ac7a60752a8ef
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-04-05 10:41:52 +0600

    implementation of func which check whether “s” is all consists of digits

--- src/Task8.java
+++ src/Task8.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class Task8 {
     public static void isDigit(){
         Scanner sc = new Scanner(System.in);
+        //input string
         String str = sc.nextLine();
+        // array of elements from string
         char[] arr = str.toCharArray();
+        //if here elements which is not digit is more than 0 => print NO
         if (isDigitFunc(arr, arr.length - 1, 0) > 0){
             System.out.println("No");
         }
@@ -14,6 +17,7 @@ public class Task8 {
     }
     public static int isDigitFunc(char[] arr, int length, int counter){
         if (length > 0){
+            //if element isn't digit => (counter of not digit element)++
             if (Character.isDigit(arr[length])){
                 counter = isDigitFunc(arr, length - 1, counter);
             }
