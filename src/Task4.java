3887661da924b89b17300bebb6c086f8b9ffd9b7
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-04-05 09:38:02 +0600

    implementation of func which finds factorial of number

--- src/Task4.java
+++ src/Task4.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class Task4 {
     public static void findFactorial(){
         Scanner sc = new Scanner(System.in);
+        //input number
         int n = sc.nextInt();
         System.out.println(findFactorialFunc(n, 1));
     }
     public static int findFactorialFunc(int n, int res){
         if (n > 1){
+            // multiplication of res and number before it
             res = findFactorialFunc(n - 1, res * n);
         }
+        //n == 1 => return 1
         return res;
     }
 }
