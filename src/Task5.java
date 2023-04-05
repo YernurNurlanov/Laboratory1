5a48a00348a11fba1090cd79392ae180e2f54ce4
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-04-05 09:47:55 +0600

    implementation of func which finds n-th elements in Fibonacci sequence

--- src/Task5.java
+++ src/Task5.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
 
 public class Task5 {
-    public static void findFibonacci(){
+    public static void findFibonacci() {
         Scanner sc = new Scanner(System.in);
+        //input number
         int n = sc.nextInt();
-        System.out.println(findFibonacciFunc(n, 0, 1, 0));
-    }
-    public static int findFibonacciFunc(int n, int x1, int x2, int res){
-        if (n > 1){
-            res = findFibonacciFunc(n-1, x2, x1 + x2, x1 + x2);
+        //0-th of fibonacci sequence is 0
+        if (n == 0) {
+            System.out.println(0);
+        } else {
+            System.out.println(findFibonacciFunc(n, 0, 1));
         }
-        return res;
+    }
+    public static int findFibonacciFunc(int n, int x1, int x2){
+        if (n > 1){
+            //find next number of Fibonacci
+            x2 = findFibonacciFunc(n-1, x2, x1 + x2);
+        }
+        return x2;
     }
 }
