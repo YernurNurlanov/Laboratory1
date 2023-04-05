616096e3e0b925aa1b4e60bb077a2d1da90f1f08
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-04-05 10:45:43 +0600

    implementation of func which finds binomial coefficient

--- src/Task9.java
+++ src/Task9.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class Task9 {
     public static void binomialCoefficient(){
         Scanner sc = new Scanner(System.in);
+        //input n and k
         int n = sc.nextInt();
         int k = sc.nextInt();
         System.out.println(binomialCoefficientFunc(n, k));
     }
     public static double binomialCoefficientFunc(int n, int k){
+        //finds coefficient by formula (n!/(k!*(n-k)!))
         return findFactorialFunc(n, 1) / findFactorialFunc(k, 1) / findFactorialFunc(n - k, 1);
     }
+    //finds factorial of needed number
     public static double findFactorialFunc(int n, double res){
         if (n > 1){
             res = findFactorialFunc(n - 1, res * n);
