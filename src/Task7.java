b72bda3fffe6704183b00433672cb6ab926a6688
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-04-05 10:24:23 +0600

    implementation of func which returns given array in reverse order

--- src/Task7.java
+++ src/Task7.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Task7 {
     public static void reverseOrder(){
         Scanner sc = new Scanner(System.in);
+        //input length of array
         int num = sc.nextInt();
         reverseOrderFunc(num);
     }
@@ -10,8 +11,10 @@ public class Task7 {
         Scanner scan = new Scanner(System.in);
         int x;
         if (num != 0) {
+            //input element of array
             x = scan.nextInt();
             reverseOrderFunc(num - 1);
+            //print element
             System.out.print(x + " ");
         }
     }
