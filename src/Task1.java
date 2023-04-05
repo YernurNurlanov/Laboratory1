d0ef7c06db802a9bfb75447a4181572c3319a6d5
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-04-05 09:14:33 +0600

    implementation of func which finds min from arr

--- src/Task1.java
+++ src/Task1.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 public class Task1 {
     public static void findMin(){
         Scanner sc = new Scanner(System.in);
+        // length of array
         int n = sc.nextInt();
         int[] N = new int[n];
+        //array filling
         for (int i = 0; i < n; i++){
             N[i] = sc.nextInt();
         }
@@ -11,10 +13,12 @@ public class Task1 {
     }
     public static int findMinFunc(int[] N, int n){
         if (n > 0){
+            // check previous element
             int min = findMinFunc(N, n - 1);
             return Math.min(min, N[n]);
         }
         else{
+            // n == 0 => return first element of array
             return N[n];
         }
     }
