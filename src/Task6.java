7a0b0dcf043d6d377c51384ec394c6a298035f86
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-04-05 10:19:09 +0600

    implementation of func which finds “a^n”

--- src/Task6.java
+++ src/Task6.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class Task6 {
     public static void findPower(){
         Scanner sc = new Scanner(System.in);
+        //input number and power of this number
         int a = sc.nextInt();
         int n = sc.nextInt();
         System.out.println(findPowerFunc(a, n, 1));
     }
     public static int findPowerFunc(int a, int n, int res) {
         if (n > 0) {
+            //multiplication of res and a (n times)
             res = findPowerFunc(a, n - 1, res * a);
         }
         return res;
