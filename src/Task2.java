2995405237637d10768b8b1010989201751cf7e5
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-04-05 09:21:09 +0600

    implementation of func which find average

--- src/Task2.java
+++ src/Task2.java
@@ -3,19 +3,23 @@ import java.util.Scanner;
 public class Task2 {
     public static void findAvg(){
         Scanner sc = new Scanner(System.in);
+        //input length of array
         int n = sc.nextInt();
         int[] N = new int[n];
+        //array filling
         for (int i = 0; i < n; i++){
             N[i] = sc.nextInt();
         }
+        //prints (sum of elements / length of array)
         System.out.println(findAvgFunc(N, n) / n);
     }
     public static double findAvgFunc(int[] N, int n){
         if (n > 1){
-            double avg = N[n - 1] + findAvgFunc(N, n - 1);
-            return avg;
+            //finds the sum of all the elements
+            return N[n - 1] + findAvgFunc(N, n - 1);
         }
         else {
+            // n == 1 => return first element of array
             return N[n - 1];
         }
     }
