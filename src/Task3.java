18b150f70e84efba7ad328cd94422ebe5b3b8ade
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-04-05 09:30:31 +0600

    implementation of func which find average

--- src/Task3.java
+++ src/Task3.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 public class Task3 {
     public static void isPrime(){
         Scanner sc = new Scanner(System.in);
+        // input number
         int n = sc.nextInt();
+        //if counter > 1 (it doesn't take into account that the number is divided by itself) => not prime
         if (isPrimeFunc(n, 0, 1) > 1){
             System.out.println("Composite");
         }
@@ -13,6 +15,7 @@ public class Task3 {
     }
     public static int isPrimeFunc(int n, int counter, int divider) {
         if (n != divider) {
+            //if n divided into divider => counter++
             if (n % divider == 0) {
                 counter = isPrimeFunc(n, counter + 1, divider + 1);
             } else {
